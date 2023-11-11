ca50481ed472eacd6951a4868e8a44ae4c7613ab
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-11 14:32:52 +0900

    chore: Add a script which initialize or update the db schema

--- metaboatrace/orm/database.py
+++ metaboatrace/orm/database.py
@@ -1,9 +1,12 @@
 import os
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
 engine = create_engine(
     os.environ.get(
-        "DATABASE_URL", "mysql+pymysql://root:password@127.0.0.1/metaboatrace?charset=utf8mb4"
+        "DATABASE_URL", "mysql+pymysql://root:password@127.0.0.1:33306/metaboatrace?charset=utf8mb4"
     )
 )
+
+Base = declarative_base()
