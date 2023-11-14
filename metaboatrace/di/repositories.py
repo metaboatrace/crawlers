56964505f17547f5d7a72cfb54f3c9cd6cdf4d0b
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-14 17:58:10 +0900

    chore: Remove unnecessary codes

--- metaboatrace/di/repositories.py
+++
@@ -1,8 +0,0 @@
-from injector import Binder, Module, singleton
-
-from ..repositories import RacerRepository
-
-
-class RepositoryModule(Module):
-    def configure(self, binder: Binder) -> None:
-        binder.bind(RacerRepository, to=RacerRepository, scope=singleton)
