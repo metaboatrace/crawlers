ef54d941850b98c31b0853a208cb5eacf1c79b6e
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-18 16:59:36 +0900

    feat: Add circumference_exhibition_records table and orm

--- metaboatrace/orm/models/race.py
+++ metaboatrace/orm/models/race.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, Date, DateTime, Integer, String, text
+from sqlalchemy import Boolean, Column, Date, DateTime, Float, Integer, String, text
 from sqlalchemy.sql.schema import ForeignKey
 
 from ..database import Base
@@ -20,3 +20,16 @@ class Race(Base):
     canceled = Column(Boolean, default=False, server_default=text("false"), nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+
+
+class CircumferenceExhibitionRecord(Base):
+    __tablename__ = "circumference_exhibition_records"
+
+    stadium_tel_code = Column(Integer, ForeignKey("stadiums.tel_code"), primary_key=True)
+    date = Column(Date, primary_key=True)
+    race_number = Column(Integer, primary_key=True)
+    pit_number = Column(Integer, primary_key=True)
+    exhibition_time = Column(Float, nullable=False)
+    exhibition_time_order = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
