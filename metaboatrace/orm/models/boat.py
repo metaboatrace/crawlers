e5dec538b634923bd681fb530927d27f3f41cc7c
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-18 17:11:45 +0900

    feat: Add motor_betting_contribute_rate_aggregations table and orm

--- metaboatrace/orm/models/boat.py
+++ metaboatrace/orm/models/boat.py
@@ -27,3 +27,15 @@ class BoatSetting(Base):
             "stadium_tel_code", "date", "race_number", "motor_number", name="uniq_index_2"
         ),
     )
+
+
+class MotorBettingContributeRateAggregation(Base):
+    __tablename__ = "motor_betting_contribute_rate_aggregations"
+
+    stadium_tel_code = Column(Integer, ForeignKey("stadiums.tel_code"), primary_key=True)
+    motor_number = Column(Integer, primary_key=True)
+    aggregated_on = Column(Date, primary_key=True)
+    quinella_rate = Column(Float, nullable=False)
+    trio_rate = Column(Float)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
