56ffea5a56e14a406cee11077cba00c00f31928e
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-19 09:30:48 +0900

    feat: Add winning_race_entries table and orm

--- metaboatrace/orm/models/race.py
+++ metaboatrace/orm/models/race.py
@@ -67,6 +67,19 @@ class StartExhibitionRecord(Base):
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
 
 
+class CircumferenceExhibitionRecord(Base):
+    __tablename__ = "circumference_exhibition_records"
+
+    stadium_tel_code = Column(Integer, ForeignKey("stadiums.tel_code"), primary_key=True)
+    date = Column(Date, primary_key=True)
+    race_number = Column(Integer, primary_key=True)
+    pit_number = Column(Integer, primary_key=True)
+    exhibition_time = Column(Float, nullable=False)
+    exhibition_time_order = Column(Integer, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
+
+
 class RaceRecord(Base):
     __tablename__ = "race_records"
 
@@ -83,15 +96,14 @@ class RaceRecord(Base):
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
 
 
-class CircumferenceExhibitionRecord(Base):
-    __tablename__ = "circumference_exhibition_records"
+class WinningRaceEntry(Base):
+    __tablename__ = "winning_race_entries"
 
     stadium_tel_code = Column(Integer, ForeignKey("stadiums.tel_code"), primary_key=True)
     date = Column(Date, primary_key=True)
     race_number = Column(Integer, primary_key=True)
     pit_number = Column(Integer, primary_key=True)
-    exhibition_time = Column(Float, nullable=False)
-    exhibition_time_order = Column(Integer, nullable=False)
+    winning_trick = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, nullable=False, onupdate=datetime.utcnow)
 
