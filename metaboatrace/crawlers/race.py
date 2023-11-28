b03ba37b58383065654231a70ec1395cc2810377
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-11-28 17:42:14 +0900

    feat: crawl boat settings

--- metaboatrace/crawlers/race.py
+++ metaboatrace/crawlers/race.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from metaboatrace.models.race import RaceInformation as RaceEntity
+from metaboatrace.models.race import BoatSetting
 from metaboatrace.models.stadium import StadiumTelCode
 from metaboatrace.scrapers.official.website.v1707.pages.race.entry_page.location import (
     create_race_entry_page_url,
@@ -11,7 +11,7 @@ from metaboatrace.scrapers.official.website.v1707.pages.race.entry_page.scraping
 )
 
 from metaboatrace.crawlers.utils import fetch_html_as_io
-from metaboatrace.repositories import RaceEntryRepository, RaceRepository
+from metaboatrace.repositories import BoatSettingRepository, RaceEntryRepository, RaceRepository
 
 
 def crawl_race_information_page(stadium_tel_code: int, date: date, race_number: int) -> None:
@@ -25,3 +25,20 @@ def crawl_race_information_page(stadium_tel_code: int, date: date, race_number: 
     race_entries = extract_race_entries(html_io)
     race_entry_repository = RaceEntryRepository()
     race_entry_repository.create_or_update_many(race_entries)
+
+    html_io.seek(0)
+    # hack: ここでやらない
+    boat_settings = [
+        BoatSetting(
+            race_holding_date=race_entry.race_holding_date,
+            stadium_tel_code=race_entry.stadium_tel_code,
+            race_number=race_entry.race_number,
+            pit_number=race_entry.pit_number,
+            boat_number=race_entry.boat_number,
+            motor_number=race_entry.motor_number,
+            motor_parts_exchanges=[],
+        )
+        for race_entry in race_entries
+    ]
+    boat_setting_repository = BoatSettingRepository()
+    boat_setting_repository.create_or_update_many(boat_settings, ["boat_number", "motor_number"])
