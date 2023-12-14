90ad5d570b22ba61baf4cc5086d801ad3a95726f
k0kishima <56298669+k0kishima@users.noreply.github.com>
2023-12-15 05:44:49 +0900

    Modify celery configuration

--- metaboatrace/celery.py
+++ metaboatrace/celery.py
@@ -1,3 +1,8 @@
 from celery import Celery
 
 app = Celery("metaboatrace", broker="redis://localhost:6379/0")
+app.conf.timezone = "UTC"
+
+import metaboatrace.crawlers.race
+import metaboatrace.crawlers.racer
+import metaboatrace.crawlers.stadium
